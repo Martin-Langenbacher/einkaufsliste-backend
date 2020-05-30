9a149ee4bfd67bf360c1c14776c13432bd51b2b4
Martin Langenbacher <martin.langenbacher@telekom.de>
2020-05-30 13:21:00 +0200

    First Commit

---
+++ src/main/java/de/martin/learning/einkaufsliste/backend/EinkaufslisteApplication.java
@@ -0,0 +1,13 @@
+package de.martin.learning.einkaufsliste.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EinkaufslisteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EinkaufslisteApplication.class, args);
+	}
+
+}
